8feada86b7e6f82db82bb2aade5b565bc9328fee
Ryan Pena <ryanspena@gmail.com>
2021-08-06 19:31:59 -0500

    password not working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,35 @@
 const router = require("express").Router();
 const { User, Post, Vote, Comment } = require("../../models");
 
+router.post("/login", (req, res) => {
+ User.findOne({
+   where: {
+     email: req.body.email,
+   },
+ }).then((dbUserData) => {
+   if (!dbUserData) {
+     res.status(400).json({ message: "No user with that email address!" });
+     return;
+   }
+
+   const validPassword = dbUserData.checkPassword(req.body.password);
+
+   if (!validPassword) {
+     res.status(400).json({ message: "Incorrect password!" });
+     return;
+   }
+
+   req.session.save(() => {
+     req.session.user_id = dbUserData.id;
+     req.session.username = dbUserData.username;
+     req.session.loggedIn = true;
+
+     res.json({ user: dbUserData, message: "You are now logged in!" });
+   });
+ });
+});
+
+
 router.get("/", (req, res) => {
   User.findAll({
     attributes: { exclude: ["password"] },
@@ -66,36 +95,9 @@ router.post("/", (req, res) => {
       res.json(dbUserData);
     });
   });
-});
-
-router.post("/login", (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email,
-    },
-  }).then((dbUserData) => {
-    if (!dbUserData) {
-      res.status(400).json({ message: "No user with that email address!" });
-      return;
-    }
-
-    const validPassword = dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res.status(400).json({ message: "Incorrect password!" });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      res.json({ user: dbUserData, message: "You are now logged in!" });
-    });
-  });
-});
-
+ });
+ 
+  
 router.put("/:id", (req, res) => {
 
   User.update(req.body, {
