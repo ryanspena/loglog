b45074c08dd98770bb5eda6189af7f5f926d23d8
Ryan Pena <ryanspena@gmail.com>
2021-08-06 19:02:20 -0500

    routes and connections established

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: "iSpy",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("   ===   Listening...   ===   "));
+});
